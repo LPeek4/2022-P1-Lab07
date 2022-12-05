3816de7c61c0f9e26e20cf436c15ca3f734cc8de
11701 <logan.peek@vtsd.com>
2022-12-05 09:22:10 -0500

    12/5/2022

--- Birds.java
+++ Birds.java
@@ -4,8 +4,13 @@ import java.applet.*;
 public class Birds {
     public static void drawBirds(Graphics g){
 
+        g.setColor(Color.black);
         g.drawArc(800,25,25,15,0,180);
-        g.drawArc()
+        g.drawArc(825,25,25,15,0,180);
+
+        g.drawArc(750,50,25,15,0,120);
+        g.drawArc(775,50,25,15,180,-120);
+
 
     }
 }
--- Boat.java
+++ Boat.java
@@ -4,6 +4,14 @@ import java.applet.*;
 public class Boat {
     public static void drawBoat(Graphics g){
 
+        g.setColor(new Color(185,125,65));
+        g.fillArc(200,150,300,250,270,-90);
+        g.fillArc(100,150,600,250,270,90);
+        g.fillRect(350,275,50,125);
+
+        g.setColor(Color.blue);
+        g.fillRect(0,325,1000,200);
+
     }
     public static void drawMasts(Graphics g){
 
