259c527db72ab8081d62d857498a93ee9c24c17b
11701 <logan.peek@vtsd.com>
2022-12-06 08:38:51 -0500

    12/6/2022

--- Boat.java
+++ Boat.java
@@ -15,11 +15,39 @@ public class Boat {
     }
     public static void drawMasts(Graphics g){
 
+        g.setColor(new Color(165,110,60));
+        g.fillRect(400,50,15,225);
+        g.fillRect(500,100,15,175);
+
     }
     public static void drawSales(Graphics g){
 
+        g.setColor(Color.white);
+        Polygon sale2 = new Polygon();
+        sale2.addPoint(515,100);
+        sale2.addPoint(515,250);
+        sale2.addPoint(625,225);
+        g.fillPolygon(sale2);
+
+        g.setColor(new Color(250,250,250));
+        Polygon sale1 = new Polygon();
+        sale1.addPoint(415,50);
+        sale1.addPoint(415,250);
+        sale1.addPoint(550,225);
+        g.fillPolygon(sale1);
+
+
     }
     public static void drawPerson(Graphics g){
+
+        g.setColor(Color.black);
+        g.fillRect(358,255,10,20);
+        g.setColor(new Color(250,210,150));
+        g.fillOval(350,245,25,25);
+        g.setColor(Color.black);
+        g.drawArc(357,258,10,5,180,180);
+        g.fillOval(355,252,4,4);
+        g.fillOval(362,252,4,4);
 
     }
 }
